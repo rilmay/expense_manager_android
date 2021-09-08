b3f038e392d0ac167cbd08e4ca1a1266b6cc4d41
reeelmay <nikita.1999.slipknot@gmail.com>
2021-09-09 00:05:36 +0300

    sms manager

---
+++ app/src/main/java/com/guzov/expensemanagercompat/SmsManager.java
@@ -0,0 +1,52 @@
+package com.guzov.expensemanagercompat;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.guzov.expensemanagercompat.entity.Sms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SmsManager {
+    public static List<Sms> getAllSms(AppCompatActivity activity) {
+        List<Sms> lstSms = new ArrayList<Sms>();
+        Sms objSms;
+        Uri message = Uri.parse("content://sms/");
+        ContentResolver cr = activity.getContentResolver();
+
+        Cursor c = cr.query(message, null, null, null, null);
+        activity.startManagingCursor(c);
+        int totalSMS = c.getCount();
+
+        if (c.moveToFirst()) {
+            for (int i = 0; i < totalSMS; i++) {
+
+                objSms = new Sms();
+                objSms.setId(c.getString(c.getColumnIndexOrThrow("_id")));
+                objSms.setAddress(c.getString(c
+                        .getColumnIndexOrThrow("address")));
+                objSms.setMsg(c.getString(c.getColumnIndexOrThrow("body")));
+                objSms.setReadState(c.getString(c.getColumnIndex("read")));
+                objSms.setTime(c.getString(c.getColumnIndexOrThrow("date")));
+                if (c.getString(c.getColumnIndexOrThrow("type")).contains("1")) {
+                    objSms.setFolderName("inbox");
+                } else {
+                    objSms.setFolderName("sent");
+                }
+
+                lstSms.add(objSms);
+                c.moveToNext();
+            }
+        }
+        // else {
+        // throw new RuntimeException("You have no SMS");
+        // }
+        c.close();
+
+        return lstSms;
+    }
+}
