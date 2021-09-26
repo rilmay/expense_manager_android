3b60b4358ac989d5b1ff3c0a1fb828b8376f3efe
reeelmay <nikita.1999.slipknot@gmail.com>
2021-09-26 23:55:06 +0300

    added configurator

---
+++ app/src/main/java/com/guzov/expensemanagercompat/chart/Configurator.java
@@ -0,0 +1,84 @@
+package com.guzov.expensemanagercompat.chart;
+
+import android.graphics.Color;
+import android.text.format.DateFormat;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+import com.guzov.expensemanagercompat.dto.EntryMessageData;
+import com.guzov.expensemanagercompat.entity.BankMessage;
+import com.guzov.expensemanagercompat.message.MessageUtils;
+
+import java.util.Date;
+import java.util.List;
+
+public class Configurator {
+    public static void prepareBarChart(BarChart chart, List<BarEntry> entries, OnChartValueSelectedListener listener) {
+        chart.clear();
+        chart.setOnChartValueSelectedListener(listener);
+        chart.setPinchZoom(false);
+        chart.setDoubleTapToZoomEnabled(false);
+        chart.setScaleYEnabled(false);
+        chart.setScaleXEnabled(true);
+        chart.animateXY(2000,2000);
+        chart.setData(getBarData(entries));
+        configureBarChartAxes(chart, entries);
+        chart.invalidate();
+    }
+
+    private static BarData getBarData(List<BarEntry> entries) {
+        BarDataSet barDataSet = new BarDataSet(entries, "");
+        barDataSet.setColors(Color.parseColor("#F0E5CF"), Color.parseColor("#C8C6C6"), Color.parseColor("#4B6587"));
+
+        final Entry[] currentLastEntry = {null};
+        barDataSet.setValueFormatter((value, entry, dataSetIndex, viewPortHandler) -> {
+            String result = "";
+            List<BankMessage> messages = entry.getData() == null ? null: ((EntryMessageData) entry.getData()).getMessages();
+            if (messages != null && !messages.isEmpty()) {
+                boolean isMessageFirstOnRow = messages.get(messages.size() - 1).getValue() == value && entry != currentLastEntry[0];
+                if (isMessageFirstOnRow) {
+                    result = String.valueOf(MessageUtils.getSummaryOfMessages(messages));
+                    currentLastEntry[0] = entry;
+                }
+            }
+            return result;
+        });
+        return new BarData(barDataSet);
+    }
+
+    private static void configureBarChartAxes(BarChart chart, List<BarEntry> entries) {
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(10f);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(false);
+        xAxis.setGranularity(1f);
+        xAxis.setValueFormatter((value, axis) -> {
+            int index = (int) value - 1;
+            String result = String.valueOf(index);
+            if (entries.size() > index) {
+                BarEntry entry = entries.get(index);
+                if (entry.getData() != null ) {
+                    Date date = ((EntryMessageData) entry.getData()).getDate();
+                    result = String.valueOf(DateFormat.format("MMM-dd", date));
+                }
+            }
+            return result;
+        });
+        YAxis yAxis = chart.getAxisLeft();
+        yAxis.setGranularity(10f);
+        yAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+    }
+}
